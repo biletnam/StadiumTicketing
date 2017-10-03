a2b5a7a13d9469aad92ebf4814c70021f92ff7c0
gr1d99 <gideonkimutai9@gmail.com>
2017-10-03 09:06:29 +0300

    Update heroku deploy requirements

--- Ticket/settings.py
+++ Ticket/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import configparser
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -182,3 +183,5 @@ EMAIL_HOST_PASSWORD = ''  # your password
 EMAIL_USE_TLS = True
 
 AUTH_USER_MODEL = 'accounts.UserProfile'
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
