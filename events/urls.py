59c037efa70fecc94c12e0dd31b56ce083c22282
gr1d99 <gideonkimutai9@gmail.com>
2017-10-05 16:42:00 +0300

    Add url routes

--- events/urls.py
+++ events/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^events$', views.EventListView.as_view(), name='event_list'),
+    url(r'^event/(?P<slug>[a-zA-Z0-9-_]+)$', views.EventDetailView.as_view(), name='event_detail'),
 ]
