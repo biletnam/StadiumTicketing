371e37d490edbc1ecae634b09dfe08bd5dbed129
gr1d99 <gideonkimutai9@gmail.com>
2017-10-05 16:42:40 +0300

    Add views for event model

--- events/views.py
+++ events/views.py
@@ -1,6 +1,7 @@
 """Contains all views of `events` app"""
 
-from django.views.generic import TemplateView
+from django.views.generic import ListView, DetailView, TemplateView
+from .models import Event
 from helpers.mixins import TitleMixin
 
 
@@ -8,3 +9,18 @@ class IndexView(TitleMixin, TemplateView):
     """Entry point view"""
     template_name = 'index.html'
     title = 'Index'
+
+    def get_context_data(self, **kwargs):
+        c = super(IndexView, self).get_context_data(**kwargs)
+        c['events'] = Event.objects.all()
+        return c
+
+
+class EventListView(ListView):
+    model = Event
+    template_name = 'event/event_list.html'
+
+
+class EventDetailView(DetailView):
+    model = Event
+    template_name = 'event/event_detail.html'
